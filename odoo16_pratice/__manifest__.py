fdef0dc5c54327ebf9ec1066606024f7c207c4f2
abdullahi <hamedjan.abdullahi2017@gmail.com>
2023-06-13 22:23:10 +0430

    add employee module and removed unneccessary files

---
+++ odoo16_pratice/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "odoo16_pratice",
+
+    'summary': """
+        Currently working on a Employee Model.""",
+
+    'description': """
+        Currently working on a Employee Model.
+    """,
+
+    'author': "Abdullahi",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/employee_views.xml',
+    ],
+}
